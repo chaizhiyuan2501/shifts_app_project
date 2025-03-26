6fcad91e6680a5a9f8047cb221df52ae0b760e06
Minamoto <chaizhiyuan2501@gmail.com>
2025-03-26 20:54:58 +0900

    utilsに曜日表示関数get_weekday_jpを追加

---
+++ backend/utils/date_utils.py
@@ -0,0 +1,9 @@
+﻿
+
+def get_weekday_jp(date):
+    """
+    指定した日付の曜日を日本語で返す（例：月、火、水、...）
+    """
+    if date is None:
+        return ""
+    return ["月", "火", "水", "木", "金", "土", "日"][date.weekday()]
