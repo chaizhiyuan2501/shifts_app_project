2278231ac6c55c1ce8e95c9aaa3c7596c4425977
kirisame19 <chaizhiyuan2501@gmail.com>
2025-05-28 00:17:58 +0900

    - フロントエンドのuserStore変更中

--- frontend/shifts_project/src/store/modules/user.ts
+++ frontend/shifts_project/src/store/modules/user.ts
@@ -1,9 +1,10 @@
 ﻿// src/store/modules/user.ts
 import { defineStore } from "pinia";
-import { reqUserLogin,reqUserRegister } from "@/api/user";
-import type { UserLoginRequest, UserLoginResponse,UserRegisterRequest } from "@/api/user/type";
+import { reqUserLogin, reqUserRegister } from "@/api/user";
+import type { UserLoginRequest, UserLoginResponse, UserRegisterRequest, UserRegisterResponse } from "@/api/user/type";
 import type { UserState } from "./type/type";
 import { SET_TOKEN, GET_TOKEN } from "@/utils/token";
+
 let useUserStore = defineStore("User", {
     state: (): UserState => {
         return {
@@ -11,28 +12,30 @@ let useUserStore = defineStore("User", {
         }
     },
     actions: {
+        // ユーザーのログイン処理を行うアクション
         async userLogin(data: UserLoginRequest) {
             let result: UserLoginResponse = await reqUserLogin(data);
             if (result.code == 200) {
-                this.token = (result.access as string);
-                SET_TOKEN((result.access as string))
+                this.token = result.access as string;
+                SET_TOKEN(result.access as string);
                 return "ok";
             } else {
                 return Promise.reject(new Error(result.data.message));
             }
         },
-        async userRegister(data:UserRegisterRequest){
-            let result:UserRegisterRequest = await reqUserRegister(data);
-            if (result.code == 200){
-                return ok;
-            }else{
-                return Promise.reject(new Error(result.data.message));
+        // ユーザーの登録処理を行うアクション
+        async userRegister(data: UserRegisterRequest) {
+            let result: UserRegisterResponse = await reqUserRegister(data);
+            if (result.code == 200) {
+                return "ok";
+            } else {
+                return Promise.reject(new Error(result.message));
             }
         }
     },
     getters: {
-
+        // ここに必要なgetterを追加可能
     }
-})
+});
 
 export default useUserStore;
